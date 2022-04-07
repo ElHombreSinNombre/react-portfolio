49f9b316635123e629b2190157fb9ac657a46f87
asier.lara <asier.lara@versia.com>
2022-04-07 10:27:25 +0200

    Placeholder data from array

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -2,46 +2,42 @@ import { useState } from "react";
 import { motion, AnimateSharedLayout, AnimatePresence } from "framer-motion";
 import "./styles.scss";
 
-export default function App() {
+
+const items = [{ name: "Project 1", icon: "angular" }, { name: "Project 2", icon: "vue" }, { name: "Project 3", icon: "laravel" }];
+
+
+export default function Projects() {
     return (
         <AnimateSharedLayout>
             <motion.ul layout initial={{ scale: 0.5, opacity: 0, borderRadius: 25 }} animate={{ scale: 1, opacity: 1 }}
                 transition={{ duration: 0.5 }}>
-                {items.map(item => (
-                    <Item key={item} />
+                {items.map((item, index) => (
+                    <Item key={index} element={item} />
                 ))}
             </motion.ul>
         </AnimateSharedLayout>
     );
 }
 
-function Item() {
+function Item(element: any) {
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = () => setIsOpen(!isOpen);
 
     return (
-        <motion.li layout onClick={toggleOpen} initial={{ borderRadius: 10 }}>
-            <motion.div className="avatar" layout />
-            <AnimatePresence>{isOpen && <Content />}</AnimatePresence>
+        <motion.li className="list-none" layout onClick={toggleOpen} initial={{ borderRadius: 10 }}>
+            <motion.img src={require('./../../assets/img/' + element.element.icon + '.ico')} layout />
+            Test
+            <AnimatePresence>{isOpen && <Content name={element.element.name} />}</AnimatePresence>
         </motion.li>
     );
 }
 
-function Content() {
+function Content(name: any) {
     return (
         <motion.div
             layout>
-            <div className="row"></div>
-            <div className="row" />
-            <div className="row" />
+            <div className="row">{name.name}</div>
         </motion.div>
     );
 }
-
-const items = [0, 1, 2];
-
-<style>
-
-
-</style>
--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -2,40 +2,47 @@ import { motion } from "framer-motion";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendar } from "@fortawesome/free-regular-svg-icons";
 
+const items = [{ text: "Work 1", description: "Test 1", date: "2022", latest: true }, { text: "Work 2", description: "Test 2", date: "2021", latest: false }, { text: "Work 3", description: "Test 3", date: "2020", latest: false }];
 
-function Timeline() {
+
+export default function TimeLine() {
     return (
         <motion.div initial={{ scale: 0.5, opacity: 0, borderRadius: 25 }} animate={{ scale: 1, opacity: 1 }}
             transition={{ duration: 0.5 }} className="shadow-lg hover:shadow-2xl duration-200 delay-75 bg-white rounded-sm py-6 pr-6 pl-9">
-        <ol className="relative border-l border-gray-200 dark:border-gray-700">
-                <li className="mb-10 ml-6">
-                    <span className="flex absolute -left-3 justify-center items-center w-6 h-6  rounded-full ring-8  dark:ring-black dark:bg-black">
-                        <FontAwesomeIcon icon={faCalendar} className="text-white" />
-                </span>
-                <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Application UI v2.0.0 <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">Latest</span></h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on January 13th, 2022</time>
-                <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">Get access to over 20+ pages including a dashboard layout, charts, kanban board, calendar, and pre-order E-commerce &amp; Marketing pages.</p>
-
-            </li>
-            <li className="mb-10 ml-6">
-                    <span className="flex absolute -left-3 justify-center items-center w-6 h-6  rounded-full ring-8 dark:ring-black dark:bg-black">
-                        <FontAwesomeIcon icon={faCalendar} className="text-white" />
-                </span>
-                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Figma v1.3.0</h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 7th, 2021</time>
-                <p className="text-base font-normal text-gray-500 dark:text-gray-400">All of the pages and components are first designed in Figma and we keep a parity between the two versions even as we update the project.</p>
-            </li>
-            <li className="mb-10 ml-6">
-                    <span className="flex absolute -left-3 justify-center items-center w-6 h-6  rounded-full ring-8 dark:ring-black dark:bg-black">
-                        <FontAwesomeIcon icon={faCalendar} className="text-white" />
-                </span>
-                <h3 className="mb-1 text-lg font-semibold text-gray-900 dark:text-white">Flowbite Library v1.2.2</h3>
-                <time className="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">Released on December 2nd, 2021</time>
-                <p className="text-base font-normal text-gray-500 dark:text-gray-400">Get started with dozens of web components and interactive elements built on top of Tailwind CSS.</p>
-            </li>
-        </ol>
+            <ol className="relative border-l border-gray-200 dark:border-gray-700">
+                {items.map((item, index) => (
+                    <Item key={index} element={item} />
+                ))}
+            </ol>
         </motion.div>
     );
 }
 
-export default Timeline;
+function Item(element: any) {
+    let latest: any;
+    if (element.element.latest == true) {
+        latest = <Latest />
+    } else {
+        latest = null;
+    }
+
+    return (
+        <li className="mb-10 ml-6">
+            <span title={element.element.date} className="cursor-help flex absolute -left-3 justify-center items-center w-6 h-6  rounded-full ring-8  dark:ring-black dark:bg-black" >
+                <FontAwesomeIcon icon={faCalendar} className="text-white" />
+            </span >
+            <h3 className="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white">{element.element.text}
+                {latest}
+            </h3>
+            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">{element.element.description}</p>
+
+        </li >
+    )
+
+}
+
+function Latest() {
+    return (
+        <span className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3" > Latest</span >
+    )
+}
