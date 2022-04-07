765e87551e4939aba1edfe8fa4586a8ecbf8356f
asier.lara <asier.lara@versia.com>
2022-04-07 15:49:51 +0200

    Header modified

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,41 +1,23 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAddressCard } from "@fortawesome/free-regular-svg-icons";
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+import { faCalendar } from "@fortawesome/free-solid-svg-icons";
+
 function Header() {
 
     const navigation = [
-        { name: 'About', href: '/', current: true },
-        { name: 'Timeline', href: '/timeline', current: false },
-        { name: 'Projects', href: '/projects', current: false },
+        { icon: <FontAwesomeIcon icon={faAddressCard} />, href: '/' },
+        { icon: <FontAwesomeIcon icon={faCalendar} />, href: '/timeline' },
+        { icon: <FontAwesomeIcon icon={faCode} />, href: '/projects' },
     ]
-    function classNames(...classes: string[]) {
-        return classes.filter(Boolean).join(' ')
-    }
 
     return (
-        <div className="bg-gray-800 hidden sm:block" >
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-                <div className="relative flex items-center justify-between h-16">
-                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="hidden sm:block sm:ml-6">
-                            <div className="flex space-x-4">
-                                {navigation.map((item) => (
-                                    <a
-                                        key={item.name}
-                                        href={item.href}
-                                        className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                            'px-3 py-2 rounded-md text-sm font-medium'
-                                        )}
-                                        aria-current={item.current ? 'page' : undefined}
-                                    >
-                                        {item.name}
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
+        <div className="bg-transparent hidden sm:block m-3" >
+            {navigation.map((item, index) => (
+                <a key={index} href={item.href} className='text-white p-4'>
+                    {item.icon}
+                </a>
+            ))}
         </div>
     )
 }
